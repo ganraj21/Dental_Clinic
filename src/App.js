7d382e2bb94dc3d731fda466602e3a2b151dbbd4
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-09-23 21:38:13 +0530

    first commet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Home from './Pages/Components/Home';
-import Navbar from './Pages/Components/navbar';
+import Navbar from './Pages/Components/Navbar';
 
 const App = () =>{
   return(
     <>
+    <h2>This is home page</h2>
     <Navbar/>
     <Home/>
     </>
