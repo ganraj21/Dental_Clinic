df80da8d68bf3ab9d571b2046731c109ab3a1c99
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-09-27 15:47:35 +0530

    service page

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ServiceData from "./ServiceData";
+import "./Services.css";
 const Services = () => {
   return (
     <>
       <div className="services_container">
         <h2>Our Services</h2>
-        <div className="service_info">
+        <div className="service_info_container">
           {ServiceData.map((e, index) => (
-            <div key={index}>
+            <div className="service_info" key={index}>
               <h2 className="service__title">{e.title}</h2>
               <p className="service_detail_info">{e.description}</p>
             </div>
