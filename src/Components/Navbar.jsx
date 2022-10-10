ef472e138650f27a0758e713a9ef7760aba85024
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-10-10 23:36:29 +0530

    dev2.0

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
   const [isActive, setActive] = useState(false);
 
   const toggleClass = () => {
-<<<<<<< HEAD
     document.getElementById("hamburger").style.display = "none";
     document.getElementById("hmcross").style.display = "flex";
     setActive(!isActive);
@@ -18,17 +17,6 @@ const Navbar = () => {
     document.getElementById("hamburger").style.display = "flex";
     document.getElementById("hmcross").style.display = "none";
     setActive(!isActive);
-=======
-    document.getElementById('hamburger').style.display = 'none';
-document.getElementById('hmcross').style.display = 'flex';
-    setActive(!isActive);
-  };
-//const [mac, setMac] = useState(toggleClass);
-  const toggleClassForClose = () => {
-       document.getElementById('hamburger').style.display = 'flex';
-       document.getElementById('hmcross').style.display = 'none';
-       setActive(!isActive);
->>>>>>> 65362179a0d761a822996f55cc34a2bc339b2c7c
   };
 
   return (
@@ -50,11 +38,7 @@ document.getElementById('hmcross').style.display = 'flex';
             </Link>
           </div>
           <div className="MenuItems">
-<<<<<<< HEAD
             <Link to="/dental-clinic/appointment" onClick={toggleClassForClose}>
-=======
-            <Link to="/dental-clinic/appointment" onClick={toggleClassForClose} >
->>>>>>> 65362179a0d761a822996f55cc34a2bc339b2c7c
               Appointment
             </Link>
           </div>
@@ -74,11 +58,7 @@ document.getElementById('hmcross').style.display = 'flex';
             <MenuIcon onClick={toggleClass} />
           </div>
           <div className="close_menu" id="hmcross">
-<<<<<<< HEAD
             <CloseIcon onClick={toggleClassForClose} />
-=======
-            <CloseIcon onClick={toggleClassForClose}/>
->>>>>>> 65362179a0d761a822996f55cc34a2bc339b2c7c
           </div>
         </div>
       </div>
