65d875f182b9210d607ebef2ded8cf7d3dd9a722
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-10-11 19:16:38 +0530

    front page dev2.0

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -57,9 +57,7 @@ const Home = () => {
             We work on holidays, besides working late on regular days. In case
             of emergencies we accept bookings.
           </p>
-          <Button variant="contained" style={{ background: "#F43E04" }}>
-            Read More
-          </Button>
+          <div className="transperent_btn">Read More</div>
         </div>
         <div className="basic_info" id="info_02">
           <h2>Best Price Guarantee</h2>
@@ -67,18 +65,16 @@ const Home = () => {
             Our reasonable prices made thousands of people smile with a new,
             beautiful, irresistible smile, as never before!!
           </p>
-          <Button variant="contained" style={{ background: "#F43E04" }}>
-            Read More
-          </Button>
+          <div className="transperent_btn">Read More</div>
         </div>
         <div className="basic_info" id="info_03">
           <h2>Opening Hours</h2>
           <p>
             Monday – Saturday: 10.00 am – 10.00 pm Sunday: 5.00 pm – 10.00 pm
           </p>
-          <Button variant="contained" style={{ background: "#F43E04" }}>
+          <div className="transperent_btn" id="tr_btn_01">
             Book An Appointment
-          </Button>
+          </div>
         </div>
       </div>
     </>
