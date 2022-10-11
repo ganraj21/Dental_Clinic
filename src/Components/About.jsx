4b8e8996ff9e925472a378fd0e83c28d1f8dcdeb
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-10-11 21:55:27 +0530

    frontpage

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import "./About.css";
+import about_img from "./images/flat-dental-care-concept-illustration_23-2149002069.jpg";
 const About = () => {
   return (
     <>
       <div className="about_section_container">
         <div className="about_container">
-          <h2 className="about_title">About US ______</h2>
-          <p className="about_short_descrp">
-            We’re glad you hand your dental health concerns to our skilled
-            hands.
-          </p>
-          <p className="about_long_descrp">
-            We’ll make sure to deliver the best possible healthcare to all our
-            patients! Of course, we always complement our main services with a
-            customer service oriented approach. We sincerely believe that
-            visiting a dentist shouldn’t be a frightening or stressful
-            experience! We provide an equally comfortable experience of
-            relaxation for all our young and adult customers! Also we implement
-            a lot of pain management and anesthesia options.
-          </p>
-          <Button variant="contained">Read More</Button>
+          <div className="about_info">
+            <h2 className="about_title">About US ______</h2>
+            <p className="about_short_descrp">
+              We’re glad you hand your dental health concerns to our skilled
+              hands.
+            </p>
+            <p className="about_long_descrp">
+              We’ll make sure to deliver the best possible healthcare to all our
+              patients! Of course, we always complement our main services with a
+              customer service oriented approach. We sincerely believe that
+              visiting a dentist shouldn’t be a frightening or stressful
+              experience! We provide an equally comfortable experience of
+              relaxation for all our young and adult customers! Also we
+              implement a lot of pain management and anesthesia options.
+            </p>
+            <Button variant="contained">Read More</Button>
+          </div>
+          <div className="about_image">
+            <img src={about_img} alt="" />
+          </div>
         </div>
       </div>
     </>
