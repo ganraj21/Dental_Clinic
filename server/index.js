d51d2ab676c5412a6fb9c6b7c59228b896dcb50b
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-10-31 17:49:38 +0530

    cors header

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ const DB = process.env.MONGO_URL;
 const app = express();
 require("dotenv").config();
 
-app.use(cors({'Access-Control-Allow-Origin': '*'}));
+app.use(cors(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }));
 app.use(express.json());
 
 app.use("/api/auth",userRoutes)
