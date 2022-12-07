318dc5bc35050e336978c51e96fddbeaf5826e10
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2022-12-07 11:17:44 +0530

    change #415

--- server/index.js
+++ server/index.js
@@ -12,13 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://ganraj21:ganraj21@cluster0.hazsbme.mongodb.net/chat?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Connection Successfully");
   })
