4575d6e3a2c786f2863a30ec6e29d53c5db0956f
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-03-04 09:45:39 +0530

    changed

--- src/Components/ApBooking.jsx
+++ src/Components/ApBooking.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { HashLink } from "react-router-hash-link";
 const apBooking = () => {
   return (
     <>
-      <div
+      <HashLink
+        to="/dental-clinic/slot"
         className="sticky_container"
         style={{
           display: "flex",
@@ -16,10 +17,11 @@ const apBooking = () => {
           zIndex: "100",
           background: "rgb(21 130 247)",
           bottom: "0",
+          textDecoration: "none",
         }}
       >
         <h3 style={{ color: "#fff", fontSize: "18px" }}>BOOK APPOINTMENT</h3>
-      </div>
+      </HashLink>
     </>
   );
 };
