343d8926ed620bb4053a891d71f7625de16a5c67
Ganesh Ghadge <89139437+ganesh6465@users.noreply.github.com>
2023-03-05 09:22:47 +0530

    stringify method

--- server/index.js
+++ server/index.js
@@ -114,7 +114,9 @@ app.post("/dental-clinic/slot", async (req, res) => {
     const userAppointment = await Appointment_info.save();
     // -----||
     if (userAppointment) {
-      const bookingDetails = `Your booking has been confirmed for the following date and time: ${userAppointment}`;
+      const bookingDetails = `Your booking has been confirmed for the following date and time: ${JSON.stringify(
+        userAppointment
+      )}`;
 
       client.messages
         .create({
